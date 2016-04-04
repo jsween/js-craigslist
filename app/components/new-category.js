cc22ae5ddc62cb7aacf9bf43754373bf0fd7a0dc
jon sweeney and yvonna contreras <yvonnap.contreras@gmail.com>
2016-04-04 11:13:41 -0700

    Create new- components

--- app/components/new-category.js
+++ app/components/new-category.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewCategory: false,
+  actions: {
+    categoryFormShow(){
+      this.set("addNewCategory", true);
+    },
+
+    saveCategory(){
+      var params = {
+        title: this.get('title')
+      };
+      this.set('addNewCategory', false);
+      this.sendAction('saveCategory', params);
+    }
+  }
 });
---
+++ app/components/new-post.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow(){
+      this.set('addNewPost', true);
+    },
+    savePost(){
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        image: this.get('image')
+      };
+      this.set('addNewPost', false);
+      this.sendAction('savePost', params);
+    }
+  }
+});
